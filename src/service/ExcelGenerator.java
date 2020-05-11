22f9b13ad4dc5b97ed027e31dcceb121c32869fc
HarishkumarSP <61948549+HarishkumarSP@users.noreply.github.com>
2020-05-11 16:38:25 +0530

    FSJ0320B005

--- src/service/ExcelGenerator.java
+++ src/service/ExcelGenerator.java
@@ -1,5 +1,5 @@
 package service;
-/*
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -11,6 +11,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import model.Prograd;
 
+
 //			Progression -1 
 //Go to src/service. Open the ExcelGenerator and fill the logic inside the excelGenerate method.
 //
@@ -21,27 +22,38 @@ public class ExcelGenerator {
 	FileOutputStream out;
 	public HSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
 		try {
-
-
-			
-			// Type your code here
-			
-		
+			String filename="D:\\.excel";
+            HSSFWorkbook hwb=new HSSFWorkbook();
+            HSSFSheet sheet=hwb.createSheet("sheet");
+            HSSFRow rowhead=sheet.createRow((short)0);
+            rowhead.createCell((short)0).setCellValue("name");
+            rowhead.createCell((short)1).setCellValue("id");
+            rowhead.createCell((short)2).setCellValue("rate");
+            rowhead.createCell((short)3).setCellValue("comment");
+            rowhead.createCell((short)4).setCellValue("recommend");
+            int i=0;
+            
+            for(Prograd st: list) {
+          	  
+          	  int j=i+1;
+          	  HSSFRow row=sheet.createRow((short)j);
+          	  row.createCell((short)0).setCellValue(st.getName());
+          	  row.createCell((short)1).setCellValue(st.getId());
+          	  row.createCell((short)2).setCellValue(st.getRate());
+          	  row.createCell((short)3).setCellValue(st.getComment());
+          	  row.createCell((short)4).setCellValue(st.getRecommend());
 			}
 			// Do not modify the lines given below
-			 out = new FileOutputStream(filename);
+			
+            out = new FileOutputStream(filename);
 			hwb.write(out);
-		
+			out.close();
 			return hwb;
 			}
 		catch (Exception e) {
 				e.printStackTrace();
 			}
-		finally {
-			out.close();
-		}
 		return null;
 		
 	}
 }
-*/
